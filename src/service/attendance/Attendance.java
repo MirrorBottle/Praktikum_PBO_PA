e643f84564bff403049bc1891909cf9e55c045b3
Alfi Nor Ihsan <alfinorihsan@gmail.com>
2023-04-17 05:10:14 +0800

    add history attendance

--- src/service/attendance/Attendance.java
+++ src/service/attendance/Attendance.java
@@ -1,7 +1,10 @@
 package service.attendance;
+import service.Service;
 import service.ServiceInterface;
 
 import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
 
 import helper.*;
 public class Attendance implements ServiceInterface {
@@ -44,8 +47,18 @@ public class Attendance implements ServiceInterface {
     Helper.keypress();
   };
 
-  public static void history() throws IOException {
-    Helper.banner("Histori Kehadiran");
+  public static void history() throws IOException, SQLException {
+    Helper.banner("History Kehadiran");
+    String[] headers = { "ID", "Status", "Tanggal/Waktu" };
+
+    ArrayList<ArrayList<String>> result = Query.select("SELECT attendances.*, users.username FROM attendances JOIN users ON attendances.user_id=users.id WHERE attendances.user_id=" + Service.authId);
+    ArrayList<ArrayList<String>> attendances = new ArrayList<>();
+    result.forEach(row -> {
+      AttendanceItem attendance = new AttendanceItem(row);
+      attendances.add(attendance.string());
+    });
+    Table table = Query.datatable(headers, attendances);
+    table.print();
     Helper.keypress();
   };
 }
